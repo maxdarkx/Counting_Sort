972ce0d7343e8fb2404b63f7ad92de9ceb90c9e6
maxdarkx <max_darkoriginal@hotmail.com>
2018-05-27 14:32:02 -0500

    max and min corrected
    correccion al hallar el valor maximo y minimo del arreglo, ahora a ordenar el arreglo

--- counting_sort.asm
+++ counting_sort.asm
@@ -1,43 +1,49 @@
 .orig x3000
 
 ld r6,data_address ;direccion del primer dato
+
 add r5,r6,#0
-
-ldr r2,r6,#0 ;en r2 va a estar guardado el mayor
 ld r4,cant 
+add r4,r4,#1
 
-max_val 	add r4,r4,#-1
-		brz fin_max
-		ldr r1,r5,#0	;utilizo el primer dato del arreglo
-		add r5,r5,#1	;le sumo uno a la direccion
-		add r0,r1,r3	;verifico si el valor que tengo es mayor (val[i]-max>0)
-		brnz max_val
+ldr r2,r5,#0
+add r3,r2,#0
+not r3,r3
+add r3,r3,#1
+
+max_val		 		add r4,r4,#0
+				brz fin_max
+				ldr r1,r5,#0	;utilizo el primer dato del arreglo
+				add r5,r5,#1	;le sumo uno a la direccion
+				add r4,r4,#-1
+				add r0,r1,r3	;verifico si el valor que tengo es mayor (val[i]-max>0)
+			brnz max_val
 		
-		add r2,r1,#0
-		not r3,r2
-		add r3,r3,#1
+			add r2,r1,#0
+			not r3,r2
+			add r3,r3,#1
 		
 		brnzp max_val
 fin_max	st r2,max
 
 add r5,r6,#0
 ld r4,cant
-ldr r2,r5,#0
+add r4,r4,#1
 
-min_val 	add r4,r4,#-1
-		brz fin_min
-		ldr r1,r5,#0	;utilizo el primer dato del arreglo
-		add r5,r5,#1	;le sumo uno a la direccion
-		add r0,r1,r3	;verifico si el valor que tengo es mayor (val[i]-max>0)
-		brp min_val
-		
-		add r2,r1,#0
-		not r3,r2
-		add r3,r3,#1
-		
+min_val				add r4,r4,#0
+				brz fin_min
+				ldr r1,r5,#0	;utilizo el primer dato del arreglo
+				add r5,r5,#1	;le sumo uno a la direccion
+				add r4,r4,#-1
+				add r0,r1,r3	;verifico si el valor que tengo es menor (val[i]-min>0)
+			brp min_val
+			
+			add r2,r1,#0
+			not r3,r2
+			add r3,r3,#1
+			
 		brnzp min_val
 fin_min	st r2,min
-
 
 halt
 
--- valores.asm
+++ valores.asm
@@ -5,8 +5,9 @@
 .fill #3099
 .fill #483
 .fill #3523
-.fill #247
-.fill #2969
-.fill #3602
 .fill #0
+.fill #2969
+.fill #-100
+.fill #-10
+.fill #5000
 .end
