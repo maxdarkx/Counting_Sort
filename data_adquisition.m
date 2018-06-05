b086dd31e1b2ebdf3c59233b53ac17f1a5a35432
maxdarkx <max_darkoriginal@hotmail.com>
2018-06-04 22:16:16 -0500

    data_adquitision_fixed
    corregido un errror en el archivo data adquisition. Ahora saca datos entre positivos y negativos y permite establecer un rango con facilidad. Continuan los problemas del commit anterior

--- data_adquisition.m
+++ data_adquisition.m
@@ -1,24 +1,24 @@
-function n=data_adquisition(string,n)
+function m=data_adquisition(string,n,nmax) 
+%n=numero de datos, nmax=rango maximo {-nmax:nmax}
     %n=1000;
-    %string='nopx';
-    n=n+1;
+    %string='nombre de archivo';
     data=zeros(n,1);
-    data(1,1)=3500;
-    for i=2:1:n-1
-        data(i,1)=randi(5000);
+    orig=3500;
+    for i=1:1:n
+        data(i,1)=randi([-nmax,nmax]);
     end
-    
+    m=data;
     string1=strcat(string,'.asm');
     text_data=fopen(string1,'w+');
    
     for i=1:1:n
         
         if i==1 
-            fprintf(text_data,'.orig x%d\n',data(i));
-        else
-            fprintf(text_data,'.fill #%d\n',data(i));
+            fprintf(text_data,'.orig x%d\n',orig);
         end
         
+        fprintf(text_data,'.fill #%d\n',data(i));
+       
         if i==n
             fprintf(text_data,'.end');
         end
@@ -28,20 +28,60 @@ function n=data_adquisition(string,n)
     fclose(text_data);
     string2=strcat(string,'.txt');
     text_data=fopen(string2,'w+');
+    
     for i=1:1:n
         if i==1
             fprintf(text_data,'int values[');
-            fprintf(text_data,'%d',n-1);
+            fprintf(text_data,'%d',n);
             fprintf(text_data,']={');
-        else
-            fprintf(text_data,'%d',data(i));
         end
         
+        fprintf(text_data,'%d',data(i));
         
         if i==n
-           fprintf(text_data,'}');
-        elseif i>1 
+           fprintf(text_data,'};');
+        elseif i>=1 
             fprintf(text_data,',');
         end
     end
+    
+    fprintf(text_data,'\n');
+    data=sort(data);
+    
+    for i=1:1:n
+        if i==1
+            fprintf(text_data,'/*int values[');
+            fprintf(text_data,'%d',n);
+            fprintf(text_data,']={');
+        end
+        
+        fprintf(text_data,'%d',data(i));
+        
+        if i==n
+           fprintf(text_data,'};//ascendente*/');
+        elseif i>=1 
+            fprintf(text_data,',');
+        end
+    end
+    
+    fprintf(text_data,'\n');
+    data=sort(data,'descend');
+    
+    for i=1:1:n
+        if i==1
+            fprintf(text_data,'/*int values[');
+            fprintf(text_data,'%d',n);
+            fprintf(text_data,']={');
+        end
+        
+        fprintf(text_data,'%d',data(i));
+        
+        if i==n
+           fprintf(text_data,'};//descendente*/');
+        elseif i>=1 
+            fprintf(text_data,',');
+        end
+    end
+    
+    fclose(text_data);
 end
